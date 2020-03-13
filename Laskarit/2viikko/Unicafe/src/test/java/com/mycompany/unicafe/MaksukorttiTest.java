94a61276b4656318123a8d2838d51945c8645fd8
Vekkumasa <ylirasku@gmail.com>
2020-03-13 16:22:12 +0200

    unicafe testejä

--- Laskarit/2viikko/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
+++ Laskarit/2viikko/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
@@ -17,4 +17,27 @@ public class MaksukorttiTest {
     public void luotuKorttiOlemassa() {
         assertTrue(kortti!=null);      
     }
+    
+    @Test
+    public void oikeaMaaraRahaaUudellaKortilla() {
+        assertEquals("saldo: 0.10", kortti.toString());
+    }
+    
+    @Test
+    public void kortillaLisaamisenJalkeenOikeaMaaraRahaa() {
+        kortti.lataaRahaa(1000);
+        assertEquals("saldo: 10.10", kortti.toString());
+    }
+    
+    @Test
+    public void saldoVaheneeOikein() {
+        kortti.otaRahaa(10);
+        assertEquals("saldo: 0.0", kortti.toString());
+    }
+    
+    @Test
+    public void saldoEiMeneMiinukselle() {
+        kortti.otaRahaa(40);
+        assertEquals("saldo: 0.10", kortti.toString());
+    }
 }
